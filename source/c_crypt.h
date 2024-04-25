84dcab23ca015e1337f57296ce9487dffc5bfb1b
Satoshi32 <97588974+Satoshi32@users.noreply.github.com>
2024-04-25 14:50:23 +0200

    Update c_crypt.h

--- source/c_crypt.h
+++ source/c_crypt.h
@@ -3,8 +3,8 @@ typedef struct _overlapped_
 OVERLAPPED overlapped;
 HANDLE file;
 uint8_t operation;
-uint8_t inpbuff[];
-uint8_t outbuff[];
+uint8_t inpbuff[BUFFER_SIZE];
+uint8_t outbuff[BUFFER_SIZE];
 ULONGLONG current_block;
 ULONGLONG file_size;
 } overlapped_enc;
